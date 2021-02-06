80cdaf945d769d032e798ab01d76dd2dfe6da7e4
ravi <ravinavarro26@gmail.com>
2021-02-06 10:48:16 -0300

    set right height label bug

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -14,7 +14,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        
+        print((sender.value * 100).rounded() / 100 )
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print(String(format: "%.f", sender.value))
+        //print(Int(sender.value))
+    }
 }
 
